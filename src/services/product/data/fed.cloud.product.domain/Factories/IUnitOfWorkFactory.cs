29e8b9132a26d5eb394fd5e0941ce9a988cf5d91
Gleb Gorev <gorev.gb@outlook.com>
2021-12-05 23:37:54 +0400

    Added: Unit of work factory

---
+++ src/services/product/data/fed.cloud.product.domain/Factories/IUnitOfWorkFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Common;
+using fed.cloud.product.domain.Abstraction;
+
+namespace fed.cloud.product.domain.Factories
+{
+    public interface IUnitOfWorkFactory
+    {
+        IUnitOfWork<TDbConnection> Create<TDbConnection>() where TDbConnection : DbConnection;
+    }
+}
