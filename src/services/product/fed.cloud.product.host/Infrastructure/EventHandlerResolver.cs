878a5bbb0eaf768d6feaae10e1a5e02642717ff4
Gleb Gorev <gorev.gb@outlook.com>
2021-12-05 23:40:26 +0400

    Added: basic things for event startup

---
+++ src/services/product/fed.cloud.product.host/Infrastructure/EventHandlerResolver.cs
@@ -0,0 +1,23 @@
+﻿using fed.cloud.eventbus.Base;
+
+namespace fed.cloud.product.host.Infrastructure;
+
+public class EventHandlerResolver : IHandlerResolver
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public EventHandlerResolver(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    
+    public IIntegrationEventHandler Resolve(string handlerType)
+    {
+        throw new NotSupportedException("resolver handler by type is not supported yet");
+    }
+
+    public IIntegrationEventHandler Resolve(Type handlerType)
+    {
+        return (IIntegrationEventHandler) _serviceProvider.GetRequiredService(handlerType);
+    }
+}
---
+++ src/services/product/fed.cloud.product.host/Infrastructure/EventServiceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using fed.cloud.common.Infrastructure;
+using fed.cloud.eventbus.Base;
+
+namespace fed.cloud.product.host.Infrastructure;
+
+public class EventServiceConfiguration : IEventServiceConfiguration
+{
+    public EventServiceConfiguration(string brokerName, string defaultConnection,
+        string defaultVectorConfig)
+    {
+        BrokerName = brokerName;
+        EventDatabase = new Database(defaultConnection, defaultVectorConfig);
+    }
+
+    public string BrokerName { get; }
+
+    public IDatabase EventDatabase { get; }
+}
