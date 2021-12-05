b9066d4fac6277964597a4c707d52bb0465fa887
Gleb Gorev <gorev.gb@outlook.com>
2021-12-05 23:41:24 +0400

    Started full registration

---
+++ src/services/product/fed.cloud.product.host/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,99 @@
+﻿using fed.cloud.product.host.Models.Configurations;
+
+namespace fed.cloud.product.host.Extensions;
+
+internal static class ConfigurationExtensions
+{
+    internal static string GetDatabaseSchema(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("Database").Get<DatabaseSection>().Schema;
+        }
+        catch
+        {
+            return "product";
+        }
+    }
+
+    internal static string GetDatabaseConnectionString(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("Database").Get<DatabaseSection>().ConnectionString;
+        }
+        catch
+        {
+            return "Host=localhost;Username=prod_user;Password=ProductQwerty123;Database=db_products";
+        }
+    }
+
+    internal static string GetDatabaseDefaultSearchVectorConfig(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("Database").Get<DatabaseSection>().DefaultSearchVectorConfig;
+        }
+        catch
+        {
+            return "english";
+        }
+    }
+
+    internal static string GetEventBrokerName(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("EventBus").Get<EventsSection>().BrokerName;
+        }
+        catch
+        {
+            return "fed_broker";
+        }
+    }
+
+    internal static string GetLocalEventsSource(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("EventBus").Get<EventsSection>().LocalEventsSource;
+        }
+        catch
+        {
+            return @"events.db";
+        }
+    }
+
+    internal static string GetEventQueueName(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("EventBus").Get<EventsSection>().QueueName;
+        }
+        catch
+        {
+            return "fed_products_queue";
+        }
+    }
+
+    internal static EventsSection GetFullEventSection(this ConfigurationManager configuration)
+    {
+        try
+        {
+            return configuration.GetSection("EventBus").Get<EventsSection>();
+        }
+        catch
+        {
+            return new EventsSection()
+            {
+                BrokerName = "fed_broker",
+                HostName = "localhost",
+                HostLogin = "guest",
+                HostPassword = "guest",
+                LocalEventsSource = "events.db",
+                MaxRetryAllowed = 5,
+                MaxTimeout = 10000
+            };
+        }
+    }
+}
--- src/services/product/fed.cloud.product.host/Program.cs
+++ src/services/product/fed.cloud.product.host/Program.cs
@@ -1,17 +1,33 @@
+using fed.cloud.product.domain.Factories;
+using fed.cloud.product.host.Extensions;
 using fed.cloud.product.host.Services;
+using fed.cloud.product.infrastructure.Factories.Implementation;
+using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
+var config = builder.Configuration;
+
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddLogging();
+builder.Services.AddSingleton<IUnitOfWorkFactory, UnitOfWorkFactory>();
+builder.Services.AddServiceConfigurations(config);
+builder.Services.AddCustomDbContext(config);
+builder.Services.AddIntegrationEvent(config);
+builder.Services.AddEventBus(config);
+builder.Services.AddMediator();
 
 var app = builder.Build();
 
+// application uses
 // Configure the HTTP request pipeline.
-//app.MapGrpcService<GreeterService>();
+app.MapGrpcService<ProductService>();
+app.MapGrpcService<SellerService>();
+app.MapGrpcService<CountryService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
