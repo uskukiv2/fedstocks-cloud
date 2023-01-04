a53330c27ed874e6d59e5f3f937fe2685e23ff37
Gleb Gorev <gorev.gb@outlook.com>
2023-01-04 23:16:50 +0400

    FED-20 New UI

--- src/web/gen.fedstocks.web.server/Models/ContextMenuLoaded.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace gen.fedstocks.web.server.Models;
-
-public class ContextMenuLoaded
-{
-    public string PageId { get; set; }
-
-    public IEnumerable<MenuItem> MenuItems { get; set; }
-}
